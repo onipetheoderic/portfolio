904a3e4ab30cbb2622607a3250319dd790c3f257
Theoderic <onipetheoderic@gmail.com>
2020-08-09 16:56:16 +0100

    Updates

---
+++ precache-manifest.0bc05e0172478919604fa20f91216863.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd4110c92538179986746d88ccbb7687",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a0d2dbab652165f3eeb3",
+    "url": "/portfolio/static/css/main.55ea0d1e.chunk.css"
+  },
+  {
+    "revision": "fa33ddf0bb27422f346d",
+    "url": "/portfolio/static/js/2.a44bf366.chunk.js"
+  },
+  {
+    "revision": "b1ccb5197627efa18ed46d5986103ce1",
+    "url": "/portfolio/static/js/2.a44bf366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d2dbab652165f3eeb3",
+    "url": "/portfolio/static/js/main.8bbe4d3a.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "1e6e8901e2eaec4e530441a944b94257",
+    "url": "/portfolio/static/media/afro.1e6e8901.png"
+  },
+  {
+    "revision": "e14a8231833cbb965636044783dd962c",
+    "url": "/portfolio/static/media/circlenode.e14a8231.png"
+  },
+  {
+    "revision": "3541431b17b93e3529f3381c0021b23d",
+    "url": "/portfolio/static/media/circlepython.3541431b.png"
+  },
+  {
+    "revision": "a5100dce0d06bda7cd12b3edfcdcdb10",
+    "url": "/portfolio/static/media/circlereact.a5100dce.png"
+  },
+  {
+    "revision": "1eb54b60108c77152ef3b04d6fc53f10",
+    "url": "/portfolio/static/media/circleruby.1eb54b60.png"
+  },
+  {
+    "revision": "3e86f6e7cb61135aa37612e300bdda06",
+    "url": "/portfolio/static/media/circularfirebase.3e86f6e7.png"
+  },
+  {
+    "revision": "8de01574327e073a6dc479b588f7d4a1",
+    "url": "/portfolio/static/media/cohimsMobile.8de01574.png"
+  },
+  {
+    "revision": "8c3170133cd440ffd2c423cddfafa284",
+    "url": "/portfolio/static/media/food.8c317013.png"
+  },
+  {
+    "revision": "2ffcded5bbce4437ca11dca77cda0152",
+    "url": "/portfolio/static/media/hdmireg.2ffcded5.png"
+  },
+  {
+    "revision": "c705afe5918f7f5fcab58ebe53789185",
+    "url": "/portfolio/static/media/lasre.c705afe5.png"
+  },
+  {
+    "revision": "ee8676514ef3787eeaccbe0e919c7cc2",
+    "url": "/portfolio/static/media/nike.ee867651.jpg"
+  },
+  {
+    "revision": "03b307138da391cf35fa704fc1bea8b7",
+    "url": "/portfolio/static/media/theo.03b30713.png"
+  }
+]);
